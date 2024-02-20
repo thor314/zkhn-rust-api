b94cb2c8a04c193d040de1256a16d0b0eb3061dc
Thor Kampefner <thorck@pm.me>
2024-02-19 17:08:10 -0800

    item and comment

--- migrations/20240219225709_comments.up.sql
+++ migrations/20240219225709_comments.up.sql
@@ -1,14 +1,18 @@
 -- Add up migration script here
 CREATE TABLE comments (
-    id VARCHAR PRIMARY KEY,
-    by VARCHAR NOT NULL,
-    parent_item_id VARCHAR NOT NULL,
-    parent_item_title VARCHAR NOT NULL,
+    id UUID PRIMARY KEY,
+    by TEXT NOT NULL,
+    parent_item_id UUID NOT NULL,
+    parent_item_title TEXT NOT NULL,
+    text TEXT NOT NULL,
     is_parent BOOLEAN NOT NULL,
-    parent_comment_id VARCHAR,
-    path TEXT, -- For Materialized Path, store as ltree or text
-    text TEXT,
-    points INTEGER DEFAULT 1 CHECK (points >= -4),
+    root_comment_id UUID NOT NULL,
+    parent_comment_id UUID,
+    children_count INT DEFAULT 0,
+    points INT DEFAULT 1 CHECK (points >= -4),
     created TIMESTAMP WITH TIME ZONE NOT NULL,
-    dead BOOLEAN DEFAULT false
+    dead BOOLEAN DEFAULT false,
+    -- references the id column of items table; when an item is deleted, cascade to children.
+    -- FOREIGN KEY (parent_item_id) REFERENCES items(id) ON DELETE CASCADE,
+    FOREIGN KEY (parent_comment_id) REFERENCES comments(id) ON DELETE CASCADE
 );
--- src/models/comment.rs
+++ src/models/comment.rs
@@ -14,13 +14,27 @@ const MIN_POINTS: i32 = -4;
 /// Comments on a post
 #[derive(sqlx::FromRow, Debug, Serialize, Deserialize)]
 pub struct Comment {
+  /// the unique identifier given to each comment in the form of a randomly generated string
   pub id:                Uuid, // Assuming UUIDs for unique identifiers, common in SQL databases
+  /// username of the user who created the comment
   pub by:                String,
-  pub parent_item_id:    String,
+  /// the id of the item the comment was placed on
+  pub parent_item_id:    Uuid,
+  /// the title of the item the comment was placed on
   pub parent_item_title: String,
+  /// body text for the comment
+  pub text:              String,
+  /// a boolean value that indicates whether or not the comment is a parent comment(not a child of
+  /// any other comment)
   pub is_parent:         bool,
+  /// a unique identifier for the root comment of a child comment, or else self
+  pub root_comment_id:   Uuid,
+  /// the id of the parent comment. This will only be added if the comment is a direct reply to
+  /// another comment
   pub parent_comment_id: Option<Uuid>,
-  pub text:              Option<String>, // todo: optional?
+  pub children_count:    i32,
+  /// sum total of upvotes and downvotes the comment has received. The minimum point value for a
+  /// comment is -4
   pub points:            i32,
   pub created:           DateTime<Utc>,
   /// Dead comments cannot be commented on, and are not displayed by default
@@ -44,35 +58,56 @@ async fn fetch_child_comments(
 }
 
 impl Comment {
-  fn new(
+  pub fn new(
     by: String,
-    parent_item_id: String,
+    parent_item_id: Uuid,
     parent_item_title: String,
     is_parent: bool,
+    root_comment_id: Option<Uuid>,
     parent_comment_id: Option<Uuid>,
-    text: Option<String>,
+    text: String,
   ) -> Self {
+    // if root_comment_id is None, then this is the root comment
+    let root_comment_id = root_comment_id.unwrap_or(Uuid::new_v4());
     Comment {
       id: Uuid::new_v4(),
       by,
       parent_item_id,
       parent_item_title,
       is_parent,
+      root_comment_id,
       parent_comment_id,
       text,
+      children_count: 0,
       points: 1,
       created: Utc::now(),
       dead: false,
     }
   }
 
-  fn edit(&mut self, text: String) { self.text = Some(text); }
+  pub fn edit(&mut self, text: String) { self.text = text; }
 
-  fn increment_point(&mut self) { self.points += 1; }
+  pub fn increment_point(&mut self) { self.points += 1; }
 
-  fn decrement_point(&mut self) { self.points = std::cmp::max(MIN_POINTS, self.points - 1); }
+  pub fn decrement_point(&mut self) { self.points = std::cmp::max(MIN_POINTS, self.points - 1); }
 
-  fn kill(&mut self) { self.dead = true }
+  // todo: set children to dead?
+  pub fn kill(&mut self) { self.dead = true }
 
-  fn unkill(&mut self) { self.dead = true }
+  pub fn unkill(&mut self) { self.dead = true }
+
+  pub fn create_child_comment(&mut self, by: String, text: String) -> Comment {
+    let comment = Comment::new(
+      by,
+      self.parent_item_id,
+      self.parent_item_title.clone(),
+      false,
+      Some(self.root_comment_id),
+      Some(self.id),
+      text,
+    );
+
+    self.children_count += 1;
+    comment
+  }
 }
