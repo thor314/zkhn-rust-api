d6c3fded4cc18a07a481c4b01c88272ff148d24b
Thor Kampefner <thorck@pm.me>
2024-02-27 08:42:22 -0800

    api init

--- api/src/api/items.rs
+++ api/src/api/items.rs
@@ -3,10 +3,10 @@ use diesel::prelude::*;
 use diesel_async::{AsyncPgConnection, RunQueryDsl};
 use uuid::Uuid as Uid;
 
-use crate::schema::{items, items::dsl::items as items_dsl};
-use crate::{
-  error::MyError,
-  // models::comment::{Comment, NewCommentPayload},
-  // schema::{comments, comments::dsl::comments as comments_dsl},
-  SharedState,
-};
+// use crate::schema::{items, items::dsl::items as items_dsl};
+// use crate::{
+//   error::MyError,
+//   // models::comment::{Comment, NewCommentPayload},
+//   // schema::{comments, comments::dsl::comments as comments_dsl},
+//   SharedState,
+// };
---
+++ api/src/lib.rs
@@ -0,0 +1,27 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unreachable_code)]
+#![allow(non_snake_case)]
+#![allow(clippy::clone_on_copy)]
+
+mod api;
+mod error;
+#[cfg(test)] mod tests;
+mod utils;
+
+use axum::{
+  http::StatusCode,
+  response::IntoResponse,
+  routing::{get, post},
+  Router,
+};
+use diesel_async::{pooled_connection::deadpool::Pool, AsyncPgConnection};
+use error::MyError;
+use shuttle_secrets::SecretStore;
+use tracing::info;
+
+#[derive(Clone)]
+pub struct SharedState {
+  pub pool: Pool<AsyncPgConnection>,
+}
