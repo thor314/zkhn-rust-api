c49ef4e34d1ab00133539f3ad22441ac29bd0dce
Thor Kampefner <thorck@pm.me>
2024-03-01 16:48:38 -0800

    tower sessions added

--- server/src/error.rs
+++ server/src/error.rs
@@ -4,13 +4,22 @@
 use thiserror::Error;
 
 #[derive(Debug, Error)]
-pub enum MyError {
+pub enum ServerError {
   #[error(transparent)]
   Anyhow(#[from] anyhow::Error),
   #[error(transparent)]
   Db(#[from] db::error::DbError),
   #[error(transparent)]
   Api(#[from] api::error::ApiError),
-  // #[error("an unhandled error")]
-  // Unhandled,
+
+  #[error(transparent)]
+  TaskJoin(#[from] tokio::task::JoinError),
+  #[error(transparent)]
+  Session(#[from] tower_sessions::session_store::Error),
+  #[error(transparent)]
+  Shuttle(#[from] shuttle_runtime::Error),
+}
+
+impl From<ServerError> for shuttle_runtime::Error {
+  fn from(e: ServerError) -> Self { shuttle_runtime::Error::Custom(e.into()) }
 }
