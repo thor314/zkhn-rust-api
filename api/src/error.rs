6bf6fe44589330952dda9616344904cfb6167f20
Thor Kampefner <thorck@pm.me>
2024-03-05 11:04:37 -0800

    http error statuscodes

--- api/src/error.rs
+++ api/src/error.rs
@@ -1,6 +1,7 @@
 //! zkhn-rust-api error types
 // https://docs.rs/thiserror/latest/thiserror/
 
+use axum::http::StatusCode;
 use db::error::DbError;
 use thiserror::Error;
 use tokio::task;
@@ -19,9 +20,28 @@ pub enum ApiError {
   DbError(#[from] DbError),
   #[error(transparent)]
   Session(#[from] tower_sessions::session_store::Error),
+  #[error(transparent)]
+  Route(#[from] RouteError),
   #[allow(dead_code)]
   #[error("an unhandled error")]
   Unhandled,
+}
+
+#[derive(Error, axum_derive_error::ErrorResponse)]
+pub enum RouteError {
+  #[status(StatusCode::NOT_FOUND)]
+  NotFound,
+  #[status(StatusCode::UNAUTHORIZED)]
+  Unauthorized,
+}
+
+impl std::fmt::Display for RouteError {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      RouteError::NotFound => write!(f, "Not Found"),
+      RouteError::Unauthorized => write!(f, "Unauthorized"),
+    }
+  }
 }
 
 #[derive(Debug, thiserror::Error)]
