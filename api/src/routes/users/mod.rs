69d269fa3e990a833a34478abc8b8b5434ab67d1
Thor Kampefner <thorck@pm.me>
2024-04-04 18:38:38 -0700

    auth bugfix 2

--- api/src/routes/users/mod.rs
+++ api/src/routes/users/mod.rs
@@ -60,9 +60,8 @@ pub(super) mod get {
     trace!("get_user called with username: {username}");
     username.validate(&())?;
     let user = users::get_assert_user(&state.pool, &username).await?;
-    let is_authenticated =
-      auth_session.get_user_from_session().map(|u| u.username == username).unwrap_or(false);
-    let user_response = GetUserResponse::new(user, is_authenticated);
+    let session_user = auth_session.get_user_from_session();
+    let user_response = GetUserResponse::new(user, session_user);
 
     debug!("user response: {user_response:?}");
     Ok(Json(user_response))
@@ -88,7 +87,7 @@ pub(super) mod get {
     auth_session: AuthSession,
   ) -> ApiResult<Json<AuthenticateUserResponse>> {
     let session_user = auth_session.get_assert_user_from_session()?;
-    let authenticate_user_response = AuthenticateUserResponse::new(session_user, true);
+    let authenticate_user_response = AuthenticateUserResponse::new(session_user);
     debug!("authenticate_user_response: {authenticate_user_response:?}");
     Ok(Json(authenticate_user_response))
   }
